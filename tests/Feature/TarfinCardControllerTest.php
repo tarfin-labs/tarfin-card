ddc8047810f7e444f9bf57485f1f57756be58ea0
Yunus Emre Deligöz <ye@deligoz.me>
2021-08-08 21:11:57 +0300

    Create TarfinCardControllerTest

---
+++ tests/Feature/TarfinCardControllerTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TarfinCardControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_customer_can_create_a_tarfin_card(): void
+    {
+        // TODO: A customer can create a Tarfin Card
+    }
+
+    /** @test */
+    public function a_customer_can_not_create_an_invalid_tarfin_card(): void
+    {
+        // TODO: A customer can not create an invalid Tarfin Card
+    }
+    
+    /** @test */
+    public function a_customer_can_see_a_tarfin_card(): void
+    {
+        // TODO: A customer can see a Tarfin Card
+    }
+    
+    /** @test */
+    public function a_customer_can_not_see_a_tarfin_card_of_another_customer(): void
+    {
+        // TODO: A customer can not see a Tarfin Card of another customer
+    }
+
+    /** @test */
+    public function a_customer_can_list_tarfin_cards(): void
+    {
+        // TODO: A customer can list Tarfin Cards
+    }
+
+    /** @test */
+    public function a_customer_can_update_a_tarfin_card(): void
+    {
+        // TODO: A customer can update a Tarfin Card
+    }
+
+    /** @test */
+    public function a_customer_can_delete_a_tarfin_card(): void
+    {
+        // TODO: A customer can delete a Tarfin Card
+    }
+
+    /** @test */
+    public function a_customer_can_activate_a_tarfin_card(): void
+    {
+        // TODO: A customer can activate a Tarfin Card
+    }
+
+    // THE MORE TESTS THE MORE POINTS :)
+}
