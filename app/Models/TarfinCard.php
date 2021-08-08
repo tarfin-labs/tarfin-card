7b44838634e229f67afccfa0fb0c5acd163fc3d3
Yunus Emre Deligöz <ye@deligoz.me>
2021-08-08 21:42:54 +0300

    TarfinCard: Add `user` relationship

--- app/Models/TarfinCard.php
+++ app/Models/TarfinCard.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class TarfinCard extends Model
 {
@@ -24,4 +25,9 @@ class TarfinCard extends Model
         'expiration_date' => 'datetime',
         'disabled_at'     => 'datetime',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
