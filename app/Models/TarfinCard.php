c9868fc4f74a79fe12243d7aa6058bf79844343e
Yunus Emre Deligöz <ye@deligoz.me>
2021-08-16 23:47:32 +0300

    TarfinCard: Add `SoftDeletes` trait

--- app/Models/TarfinCard.php
+++ app/Models/TarfinCard.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TarfinCard extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     // region Attributes
 
