1208b0b65e1152aeb9aea0b7a6e3d9bbc05073f2
Yunus Emre Deligöz <ye@deligoz.me>
2021-08-20 00:27:52 +0300

    Create `TarfinCardTransactionResource`

---
+++ app/Http/Resources/TarfinCardTransactionResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TarfinCardTransactionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return array
+     */
+    public function toArray($request): array
+    {
+        return [
+            'amount'        => $this->amount,
+            'currency_code' => $this->currency_code,
+        ];
+    }
+}
