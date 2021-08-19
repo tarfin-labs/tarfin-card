6744b4b7e815ee1240bd9d234693bc433b517ac0
Yunus Emre Deligöz <ye@deligoz.me>
2021-08-20 01:34:32 +0300

    TarfinCardTransactionController: Add `store()` method

--- app/Http/Controllers/TarfinCardTransactionController.php
+++ app/Http/Controllers/TarfinCardTransactionController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TarfinCardTransactionCreateRequest;
 use App\Http\Requests\TarfinCardTransactionViewAnyRequest;
 use App\Http\Resources\TarfinCardTransactionResource;
 use App\Models\TarfinCard;
-use App\Models\TarfinCardTransaction;
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class TarfinCardTransactionController extends Controller
@@ -14,13 +13,28 @@ class TarfinCardTransactionController extends Controller
     /**
      * Display a listing of the TarfinCardTransaction for the given TarfinCard.
      *
-     * @param  \App\Http\Controllers\TarfinCardTransactionViewAnyRequest  $request
-     * @param  \App\Models\TarfinCard                                     $tarfinCard
+     * @param  \App\Http\Requests\TarfinCardTransactionViewAnyRequest  $request
+     * @param  \App\Models\TarfinCard                                  $tarfinCard
      *
      * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function index(TarfinCardTransactionViewAnyRequest $request, TarfinCard $tarfinCard): AnonymousResourceCollection
     {
         return TarfinCardTransactionResource::collection($tarfinCard->transactions);
+    }
+
+    /**
+     * Store a newly created TarfinCardTransaction in storage.
+     *
+     * @param  \App\Http\Requests\TarfinCardTransactionCreateRequest  $request
+     * @param  \App\Models\TarfinCard                                 $tarfinCard
+     *
+     * @return \App\Http\Resources\TarfinCardTransactionResource
+     */
+    public function store(TarfinCardTransactionCreateRequest $request, TarfinCard $tarfinCard): TarfinCardTransactionResource
+    {
+        $newTarfinCardTransaction = $tarfinCard->transactions()->create($request->validated());
+
+        return new TarfinCardTransactionResource($newTarfinCardTransaction);
     }
 }
