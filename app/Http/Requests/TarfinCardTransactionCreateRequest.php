3b5d9215fa79df9409390604050523d2ad353259
Yunus Emre Deligöz <ye@deligoz.me>
2021-08-20 01:32:58 +0300

    Create `TarfinCardTransactionCreateRequest`

---
+++ app/Http/Requests/TarfinCardTransactionCreateRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\TarfinCardTransaction;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class TarfinCardTransactionCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->can('create', [TarfinCardTransaction::class, $this->route('tarfin_card')]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'amount'        => ['required', 'integer'],
+            'currency_code' => ['required', 'string', Rule::in(TarfinCardTransaction::CURRENCIES)],
+        ];
+    }
+}
