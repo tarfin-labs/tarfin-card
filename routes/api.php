e790b4eeb5926f0d6da18352e66c26ffd49189c7
Yunus Emre Deligöz <ye@deligoz.me>
2021-08-20 00:30:14 +0300

    api: Add `tarfin-cards.tarfin-card-transactions` API resource

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
 use App\Http\Controllers\TarfinCardController;
+use App\Http\Controllers\TarfinCardTransactionController;
 
 Route::apiResource('tarfin-cards', TarfinCardController::class);
+Route::apiResource('tarfin-cards.tarfin-card-transactions', TarfinCardTransactionController::class)
+    ->only(['index', 'show', 'store'])
+    ->shallow();
