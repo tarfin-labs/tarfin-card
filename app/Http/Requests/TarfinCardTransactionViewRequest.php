b7d4fd7660fb80ce4235b6b53949d824d9713e17
Yunus Emre Deligöz <ye@deligoz.me>
2021-08-20 01:45:15 +0300

    Create `TarfinCardTransactionViewRequest`

---
+++ app/Http/Requests/TarfinCardTransactionViewRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TarfinCardTransactionViewRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->can('view', $this->route('tarfin_card_transaction'));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [];
+    }
+}
