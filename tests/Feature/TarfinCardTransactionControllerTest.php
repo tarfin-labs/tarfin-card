de83e0450ac68a5e625b7331b5a3eec0a4fdfa66
Yunus Emre Deligöz <ye@deligoz.me>
2021-08-20 00:54:18 +0300

    Create `TarfinCardTransactionControllerTest`

---
+++ tests/Feature/TarfinCardTransactionControllerTest.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TarfinCardTransactionControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_customer_can_create_a_tarfin_card_transaction(): void
+    {
+        $this->markTestSkipped();
+
+        // region 1️⃣ Arrange 🏗
+        // TODO:
+        // endregion
+
+        // region 2️⃣ Act 🏋🏻‍
+        // TODO:
+        // endregion
+
+        // region 3️⃣ Assert ✅
+        // TODO:
+        // endregion
+    }
+
+    /** @test */
+    public function a_customer_can_not_create_a_tarfin_card_transaction_for_a_tarfin_card_of_another_customer(): void
+    {
+        $this->markTestSkipped();
+
+        // region 1️⃣ Arrange 🏗
+        // TODO:
+        // endregion
+
+        // region 2️⃣ Act 🏋🏻‍
+        // TODO:
+        // endregion
+
+        // region 3️⃣ Assert ✅
+        // TODO:
+        // endregion
+    }
+
+    /** @test */
+    public function a_customer_can_see_a_tarfin_card_transaction(): void
+    {
+        $this->markTestSkipped();
+
+        // region 1️⃣ Arrange 🏗
+        // TODO:
+        // endregion
+
+        // region 2️⃣ Act 🏋🏻‍
+        // TODO:
+        // endregion
+
+        // region 3️⃣ Assert ✅
+        // TODO:
+        // endregion
+    }
+
+    /** @test */
+    public function a_customer_can_not_see_a_tarfin_card_transaction_for_a_tarfin_card_of_another_customer(): void
+    {
+        $this->markTestSkipped();
+
+        // region 1️⃣ Arrange 🏗
+        // TODO:
+        // endregion
+
+        // region 2️⃣ Act 🏋🏻‍
+        // TODO:
+        // endregion
+
+        // region 3️⃣ Assert ✅
+        // TODO:
+        // endregion
+    }
+
+    /** @test */
+    public function a_customer_can_list_tarfin_card_transactions(): void
+    {
+        $this->markTestSkipped();
+
+        // region 1️⃣ Arrange 🏗
+        // TODO:
+        // endregion
+
+        // region 2️⃣ Act 🏋🏻‍
+        // TODO:
+        // endregion
+
+        // region 3️⃣ Assert ✅
+        // TODO:
+        // endregion
+    }
+
+    /** @test */
+    public function a_customer_can_not_list_tarfin_card_transactions_for_a_tarfin_card_of_another_customer(): void
+    {
+        $this->markTestSkipped();
+
+        // region 1️⃣ Arrange 🏗
+        // TODO:
+        // endregion
+
+        // region 2️⃣ Act 🏋🏻‍
+        // TODO:
+        // endregion
+
+        // region 3️⃣ Assert ✅
+        // TODO:
+        // endregion
+    }
+
+    // THE MORE TESTS THE MORE POINTS :)
+}
