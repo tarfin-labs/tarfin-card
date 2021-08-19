ac4a0a46caf59ddfe603b8c8f71ef91fc6cddb01
Yunus Emre Deligöz <ye@deligoz.me>
2021-08-20 00:27:40 +0300

    Create `TarfinCardTransactionFactory`

---
+++ database/factories/TarfinCardTransactionFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TarfinCard;
+use App\Models\TarfinCardTransaction;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TarfinCardTransactionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TarfinCardTransaction::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'tarfin_card_id' => TarfinCard::factory(),
+            'amount'         => $this->faker->numberBetween(100, 999) * 10,
+            'currency_code'  => $this->faker->randomElement(TarfinCardTransaction::CURRENCIES),
+        ];
+    }
+}
