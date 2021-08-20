a345f70526b63a38cc27afea83088445239e3f04
Yunus Emre Deligöz <ye@deligoz.me>
2021-08-20 09:40:25 +0300

    Create `ComposerPostUpdateHandlerCommand`

---
+++ app/Console/Commands/ComposerPostUpdateHandlerCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ComposerPostUpdateHandlerCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'post-update:handle';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Handle Composer post-update event for ide-helper';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        if (app()->environment(['local'])) {
+            $this->call('ide-helper:generate', ['--quiet' => true]);
+            $this->call('ide-helper:meta', ['--quiet' => true]);
+            $this->call('ide-helper:models', ['--nowrite' => true, '--write-mixin' => true, '--quiet' => true]);
+            $this->call('ide-helper:eloquent', ['--quiet' => true]);
+        }
+    }
+}
