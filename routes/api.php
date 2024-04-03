d699e51f8136e0f23cf2bf36a67cce8809458333
Yunus Emre Deligöz <ye@deligoz.me>
2024-04-04 01:00:49 +0300

    Remove unnecessary API route comments
    The comment block explaining API route registrations was deleted from the code. These comments were redundant and not required as the code's purpose was self-explanatory. This action streamlines the code and enhances readability.

--- routes/api.php
+++ routes/api.php
@@ -5,17 +5,6 @@ declare(strict_types=1);
 use App\Http\Controllers\TarfinCardController;
 use App\Http\Controllers\TarfinCardTransactionController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::apiResource(
     name: 'tarfin-cards',
     controller: TarfinCardController::class
