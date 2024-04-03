4b5ea90595524bf0e612b90a727d80417862ad01
Yunus Emre Deligöz <ye@deligoz.me>
2024-04-04 00:11:09 +0300

    Refactor UserFactory and improve comment clarity
    Updated the method of generating 'remember_token' to use named arguments in UserFactory. Also, improved the 'unverified' function comment for better understanding and refactored its code block for simplicity.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,19 +27,18 @@ class UserFactory extends Factory
             'email'             => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token'    => Str::random(10),
+            'remember_token'    => Str::random(length: 10),
         ];
     }
 
     /**
-     * Indicate that the model's email address should be unverified.
+     * Return an instance of the Factory with email_verified_at set to null.
+     *
      */
     public function unverified(): Factory
     {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
+        return $this->state(fn(): array => [
+            'email_verified_at' => null,
+        ]);
     }
 }
