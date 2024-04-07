26d22ed98e07c7aa873d181cd146a9600d3d8f09
Yunus Emre Deligöz <ye@deligoz.me>
2024-04-07 22:21:10 +0300

    refactor(UserFactory): use explicit parameter names in method calls
    This commit replaces implicit parameter names with explicit ones in the UserFactory class, which improves code clarity.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,8 +27,8 @@ class UserFactory extends Factory
             'name'              => fake()->name(),
             'email'             => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password'          => static::$password ??= Hash::make('password'),
-            'remember_token'    => Str::random(10),
+            'password'          => static::$password ??= Hash::make(value: 'password'),
+            'remember_token'    => Str::random(length: 10),
         ];
     }
 
