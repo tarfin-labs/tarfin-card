f8a2c9e1056e6fa7a4bdc37c2235178c3a535a01
Yunus Emre Deligöz <ye@deligoz.me>
2024-05-19 23:02:53 +0300

    test(TarfinCardTransactionControllerTest): Reduce test cases

--- tests/Feature/Controllers/TarfinCardTransactionControllerTest.php
+++ tests/Feature/Controllers/TarfinCardTransactionControllerTest.php
@@ -26,59 +26,7 @@ class TarfinCardTransactionControllerTest extends TestCase
     }
 
     #[Test]
-    public function a_customer_can_not_create_a_tarfin_card_transaction_for_a_tarfin_card_of_other_customer(): void
-    {
-        // 1. Arrange
-        // TODO: Implement the `Arrange` part
-
-        // 2. Act
-        // TODO: Implement the `Act` part
-
-        // 3. Assert
-        // TODO: Implement the `Assert` part
-    }
-
-    #[Test]
-    public function a_customer_can_see_a_tarfin_card_transaction(): void
-    {
-        // 1. Arrange
-        // TODO: Implement the `Arrange` part
-
-        // 2. Act
-        // TODO: Implement the `Act` part
-
-        // 3. Assert
-        // TODO: Implement the `Assert` part
-    }
-
-    #[Test]
     public function a_customer_can_not_see_a_tarfin_card_transaction_for_a_tarfin_card_of_other_customer(): void
-    {
-        // 1. Arrange
-        // TODO: Implement the `Arrange` part
-
-        // 2. Act
-        // TODO: Implement the `Act` part
-
-        // 3. Assert
-        // TODO: Implement the `Assert` part
-    }
-
-    #[Test]
-    public function a_customer_can_list_tarfin_card_transactions(): void
-    {
-        // 1. Arrange
-        // TODO: Implement the `Arrange` part
-
-        // 2. Act
-        // TODO: Implement the `Act` part
-
-        // 3. Assert
-        // TODO: Implement the `Assert` part
-    }
-
-    #[Test]
-    public function a_customer_can_not_list_tarfin_card_transactions_for_a_tarfin_card_of_other_customer(): void
     {
         // 1. Arrange
         // TODO: Implement the `Arrange` part
